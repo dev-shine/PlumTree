f13b060b54e5b8cb846f8dea47edfb01669340cb
robbieve <robbieeve751994@gmail.com>
2019-01-17 22:14:03 -0500

    structure

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
+
+
+injectTapEventPlugin();
+
+ReactDOM.render(
+    <AppContainer>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </AppContainer>,
+    document.getElementById('root')
+);
+registerServiceWorker();
